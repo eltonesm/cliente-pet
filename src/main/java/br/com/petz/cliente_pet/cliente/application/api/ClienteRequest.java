cce43392330712160c64468311bac5586f42403a
eltonesm <eltonesm@gmail.com>
2024-06-18 22:51:10 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,32 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.Sexo;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    @NotBlank
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
 }
--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteResponse.java
@@ -1,4 +1,8 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import java.util.UUID;
+
 public class ClienteResponse {
+
+    private UUID idCliente;
 }
