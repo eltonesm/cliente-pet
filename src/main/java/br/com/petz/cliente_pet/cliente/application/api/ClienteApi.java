3a164cbad417edca6d7b6baf08788e355b6c529a
eltonesm <eltonesm@gmail.com>
2024-07-03 16:55:56 -0300

    Implementa api get cliente atraves do Id

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteApi.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/cliente")
@@ -17,4 +18,10 @@ public interface ClienteApi{
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     List<ClienteListResponse> getTodosClientes();
+
+    @GetMapping(value = "/{idCliente}")
+    @ResponseStatus(code = HttpStatus.OK)
+    ClienteDetalhadoResponse getClienteAtravesId(@PathVariable UUID idCliente);
+
+
 }
--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Log4j2
@@ -28,5 +29,13 @@ public class ClienteController implements ClienteApi {
         List<ClienteListResponse> clientes = clienteService.buscaTodosClientes();
         log.info("[Finaliza] ClienteController - getTodosClientes");
         return clientes;
+    }
+
+    @Override
+    public ClienteDetalhadoResponse getClienteAtravesId(UUID idCliente) {
+        log.info("[Inicia] ClienteController - getClienteAtravesId");
+        log.info("[idCliente]{}", idCliente);
+        log.info("[Finaliza] ClienteController - getClienteAtravesId");
+        return null;
     }
 }
